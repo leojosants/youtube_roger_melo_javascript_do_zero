4e481bc83f5d7e42c9ebc38804ccee2c5521d026
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-24 16:19:51 -0300

    Aula 4 - Ponto e vírgula, string e template string

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
-const anoAtual = 2023;
+const anoAtual = 2024;
 const anoNascimento = 1984;
 const idade = anoAtual - anoNascimento;
-const novoAnoNascimento = anoNascimento + 1;
-const novaIdade = anoAtual - novoAnoNascimento;
+const nome = 'Leonardo';
 
-console.log(novaIdade);
+// aspas simples - ''
+// const frase = 'Meu nome é ' + nome + ' e tenho ' + idade + ' anos.';
+
+// aspas duplas - ""
+// const frase = "Meu nome é " + nome + " e tenho " + idade + " anos.";
+
+// template strings - ``
+const frase = `Meu nome é ${nome} e tenho ${idade} anos.`;
+
+// escape de caracteres - \\
+// const frase2 = "Ela disse \"Olá\"!";
+const frase2 = `Ela disse "Olá"!`;
+
+console.log(frase);
+console.log(frase2);
+console.log(nome[0]);
