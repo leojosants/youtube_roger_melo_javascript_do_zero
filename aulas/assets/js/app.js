03ab9e1cd5f33f31a49322182e9372b773f81af1
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-24 21:37:41 -0300

    Aula 8 - Imutabilidade e estrutura de dados objeto

--- aulas/assets/js/app.js
+++ aulas/assets/js/app.js
@@ -1,27 +1,33 @@
-/*
-    tipos primitivos
-        - number
-        - null
-        - undefined
-        - boolean
-        - string
-        - bigint
-        - symbol
-        - objeto
-            - objetos literais
-            - arrays
-            - funções
-*/
-
-const microfone = {
-    cor: 'preto',
-    marca: 'Fifine',
-    preco: 397.97,
+const serie = {
+    nome: 'Game of Thrones',
+    temporadas: 8,
 };
 
-const corMicrofone = microfone.cor;
-const marcaMicrofone = microfone.marca;
-const precoMicrofone = microfone.preco;
-const novoPrecoMicrofone = (microfone.preco - 100);
+// atribuição
+serie.temporadas = 10;
 
-console.log(novoPrecoMicrofone);
+// Uncaught TypeError: Assignment to constant variable.
+// serie = 'filme';
+
+// criação
+serie.episodios = 73;
+
+const frase = `
+A série ${serie.nome} tem ${serie.temporadas} temporadas e ${serie.episodios} episódios.
+`;
+
+const serie2 = {};
+
+serie2.nome = 'Game of Thrones';
+serie2.episodios = 53;
+serie2.temporadas = 6;
+
+const pessoa = {
+    nome: 'Leonardo',
+    peso: 76,
+    altura: 1.70,
+    idade: 39,
+}
+
+console.log(frase);
+console.log(serie2);
