c7e458208cbdcb4d01ebdffb56281d6ad93ac086
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-26 14:00:28 -0300

    Aula 24 - Aprenda for loop, push e propriedade length

--- aulas/assets/js/app.js
+++ aulas/assets/js/app.js
@@ -1,62 +1,18 @@
-/* operador && 
-    const valorTruthy1 = 1;
-    const valorFalsy0 = 0;
+const numeros = [10, 20, 30, 40, 50];
+const somaNumerosSemFOR = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4];
+let somaNumerosComFOR = 0;
 
-    // quando o valor a esquerda do && é um valor truthy, o valor a direita do && é retornado
-    const resultado1 = valorTruthy1 && valorFalsy0;
+console.log('Elementos do array........:', numeros);
+console.log('Tamanho do array..........:', numeros.length);
 
-    // quando o valor a esquerda do && é um valor falsy, o valor a esquerda do && é retornado
-    const resultado2 = valorFalsy0 && valorTruthy1;
-
-    console.log(resultado1);
-    console.log(resultado2);
-*/
-
-/* operador || 
-    const valorTruthy1 = 1;
-    const valorFalsy0 = 0;
-
-    // quando o valor a esquerda do || é um valor truthy, o valor a esquerda do || é retornado
-    const resultado1 = valorTruthy1 || valorFalsy0;
-
-    // quando o valor a esquerda do || é um valor falsy, o valor a direita do || é retornado
-    const resultado2 = valorFalsy0 || valorTruthy1;
-
-    console.log(resultado1);
-    console.log(resultado2);
-*/
-
-/* operador ternário - expressão: condição ? valor_1 : valor_2 
-    const x = 7;
-    const y = 7;
-
-    console.log('x:', x);
-    console.log('y:', y);
-
-    const resultado1 = (x === y ? 'x é igual a y' : 'x é diferente de y');
-    const resultado2 = (x !== y ? 'x é igual a y' : 'x não é diferente de y');
-
-    console.log(resultado1);
-    console.log(resultado2);
-*/
-
-/* operador ternário - expressão: condição ? valor_1 : valor_2 */
-const pessoa = {
-    nome: 'Leo',
-    genero: 'Masculino',
-};
-
-// ternário
-// const agradecimento = (pessoa.genero === 'Masculino' ? 'Obrigad(o)' : 'Obrigad(a)');
-
-// if tradicional
-let agradecimento;
-
-if (pessoa.genero === 'Masculino') {
-    agradecimento = 'Obrigad(o)';
-}
-else {
-    agradecimento = 'Obrigad(a)';
+for (let i = 0; i < numeros.length; i++) {
+    somaNumerosComFOR += numeros[i];
 }
 
-console.log(agradecimento);
+numeros.push(6);
+
+console.log('Elementos do array........:', numeros);
+console.log('Tamanho do array..........:', numeros.length);
+
+console.log('Soma dos números sem FOR..:', somaNumerosSemFOR);
+console.log('Soma dos números com FOR..:', somaNumerosComFOR);
