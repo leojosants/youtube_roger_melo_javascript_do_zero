a0b7f8fc65860db6b46f7e9226452d192a2eec1f
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-27 00:45:08 -0300

    Aula 28 - Armadilhas entre tipo objeto e primitivos

--- aulas/assets/js/app.js
+++ aulas/assets/js/app.js
@@ -1,26 +1,96 @@
-/*
-    const numeros = [10, 20, 30];
+/* imutável
+    const mes = ['janeiro'];
 
-    console.log('Números..:', numeros);
-    console.log('10 existe?', numeros.includes(10));
-    console.log('20 existe?', numeros.includes(20));
-    console.log('30 existe?', numeros.includes(30));
-    console.log('40 existe?', numeros.includes(40));
+    console.log('Mês:', mes);
+    console.log(mes[0][0].toLowerCase());
+    console.log(mes[0][0] = 'J');
+    console.log('Mês:', mes);
+
+    mes[0] = 'Janeiro';
+
+    console.log('Mês:', mes);
 */
 
-/*
-    const mensagem = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tempora commodi voluptatem quidem corrupti neque autem non amet, culpa provident sapiente aperiam voluptate ipsum asperiores natus quibusdam fugit atque magni!`;
+/* imutável
+    const mensagem = 'Eu sou Médico!';
 
-    console.log('Mensagem......:', mensagem);
-    console.log(`'fugit' existe?`, mensagem.includes('fugit'));
-    console.log(`'teste' existe?`, mensagem.includes('teste'));
+    console.log('Mensagem.......:', mensagem);
+
+    const novaMensagem = mensagem.replace('Médico', 'Desenvolvedor');
+
+    console.log('Nova mensagem..:', novaMensagem);
+    console.log('Mensagem.......:', mensagem);
 */
 
-const strings = ['1', '1', '2'];
+/* mutável
+    const array = ['a', 'c', 'c'];
 
-console.log('Strings:', strings);
+    console.log('Array:', array);
 
-// checar se todos os itens do array correponde a uma comparação
-const resultado = strings.every((string) => string === '1');
+    array[1] = 'b';
 
-console.log(`Todos os elementos do array 'strings' são iguais a '1'?`, resultado);
+    console.log('Array:', array);
+*/
+
+/* mutável
+    const obj = {
+        nome: 'Leo',
+    };
+
+    console.log('Obj:', obj);
+
+    obj.nome = 'Leonardo';
+
+    console.log('Obj:', obj);
+*/
+
+/* passagem de valor - valor 
+    const x = {
+        prop: 1,
+    };
+
+    const y = {
+        prop: 1,
+    };
+
+    console.log('x......:', x);
+    console.log('y......:', y);
+    console.log('x === y:', x === y);
+*/
+
+/* passagem de valor - referência 
+    const x1 = {
+        prop: 1,
+    };
+
+    const y1 = x1;
+
+    console.log('x1.......:', x1);
+    console.log('y1.......:', y1);
+    console.log('x1 === y1:', x1 === y1);
+
+    y1.prop = 3;
+
+    console.log('x1.......:', x1);
+    console.log('y1.......:', y1);
+*/
+
+/* spread */
+const x1 = {
+    prop: 1,
+}
+
+const y1 = {
+    ...x1,
+    prop: 0,
+    prop1: 1,
+};
+
+console.log('x1.........:', x1);
+console.log('y1.........:', y1);
+console.log('x1 === y1..:', x1 === y1);
+
+y1.prop = 3;
+
+console.log('x1.........:', x1);
+console.log('y1.........:', y1);
